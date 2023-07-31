b8fb6abf4df6c91966d5970ff84feb6557a99a03
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-07-31 22:21:01 +0200

    added extensions

---
+++ XFrame.Jobs/Extensions/JobExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Reflection;
+using XFrame.Common.Extensions;
+
+namespace XFrame.Jobs.Extensions
+{
+    public static class JobExtensions
+    {
+        public static void AddInstantJobScheduler(
+            this IServiceCollection services)
+        {
+            services.TryAddTransient<IJobScheduler, InstantJobScheduler>();
+        }
+
+        public static void AddJobRunner(
+            this IServiceCollection services)
+        {
+            services.TryAddTransient<IJobRunner, JobRunner>();
+        }
+
+        public static IEnumerable<Type> GetJobs(
+            this Assembly assembly,
+            Predicate<Type> predicate = null)
+        {
+            predicate = predicate ?? (t => true);
+            var jobTypes = assembly
+                .GetTypes()
+                .Where(type => !type.GetTypeInfo().IsAbstract && type.IsAssignableTo<IJob>())
+                .Where(t => !t.HasConstructorParameterOfType(i => i.IsAssignableTo<IJob>()))
+                .Where(t => predicate(t));
+            return jobTypes;
+        }
+    }
+}
